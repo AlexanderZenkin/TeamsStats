5ecb85d1358463d928a345f0a08b751ff3218901
AlexanderZenkin <aidik17@yandex.ru>
2022-11-09 12:02:10 +0300

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/teamsstats/ExampleUnitTest.java
+++ app/src/test/java/com/example/teamsstats/ExampleUnitTest.java
@@ -2,7 +2,15 @@ package com.example.teamsstats;
 
 import static org.junit.Assert.assertEquals;
 
+import com.example.teamsstats.interfaces.APIService;
+
 import org.junit.Test;
+
+import java.io.IOException;
+
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +21,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testRetrofit() throws IOException {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.football-data.org/v4/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+
+        APIService userService = retrofit.create(APIService.class);
+        Request test = userService.getUser("2021").execute().raw().request();
+
+        System.out.println("stop");
     }
 }
