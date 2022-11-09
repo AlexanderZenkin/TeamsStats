1ace25487afead4640d353f160f19df4c3ca2e9d
AlexanderZenkin <aidik17@yandex.ru>
2022-11-09 12:02:03 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/teamsstats/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/teamsstats/ExampleInstrumentedTest.java
@@ -8,9 +8,14 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.example.teamsstats.interfaces.APIService;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 
 /**
